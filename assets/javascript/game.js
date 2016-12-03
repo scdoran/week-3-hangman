a295cfc7beaf02afb9c0cea47befbaae4de9ef47
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2016-12-02 19:24:56 -0500

    Here is most of the psychic code completed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var guesses = [];
-
-var wins = 0;
-
-var losses = 0; 
-
-var guessesleft = 12;
+var game = {
+	wins: 0,
+	losses: 0, 
+	guessesLeft: 9,
+}
 
 document.onkeyup = function (event) {
 
@@ -18,20 +16,26 @@ document.onkeyup = function (event) {
 
 		if (userGuess === computerGuess) {
 			wins++;
-			alert("You win! Time to see if you can...stump my log.");
-			var win = 0;
-			var losses = 0;
-			var guessesleft = 12; 
+			document.getElementById("guesses").textContent += event.key + " ";
+			game.guessesLeft = 9;
+			alert("You win! The letter was " + computerGuess + "Would you like to see if you can be stumped by my log again?");
 		}
 
 		else if (userGuess !== computerGuess) {
-			losses++;
-			guessesleft--;
-		}
+			game.guessesLeft--;
+			console.log("You guessed " + event.key + " ");
+			document.getElementById("guesses").textContent += event.key + " ";
+		}	
+		
+		else if (game.guessesLeft === 0) {
+			alert("You have been...stumped by my log. The letter you were trying to guess was " + computerGuess + " Would you like to try again?");
+			document.getElementById("guesses").textContent += event.key + " ";
+			game.losses = 1;
+			game.guessesLeft = 9;
+		}		
 	}
 
-	document.getElementById("wins").innerHTML = wins;
-	document.getElementById("losses").innerHTML = losses;
-	document.getElementById("guesses").innerHTML += even.key; 
-	document.getElementById("guesses-left").innerHTML = guesses
-};
+	document.getElementById("wins").innerHTML = game.wins;
+	document.getElementById("losses").innerHTML = game.losses;
+	document.getElementById("guesses-left").innerHTML = game.guessesLeft;
+}
