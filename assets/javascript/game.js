aebe5541375062a16d4c4f73ece3dd99b8327d7e
Sarah Doran <sarahdoran@Sarahs-MacBook-Air-2.local>
2016-12-04 15:19:05 -0500

    Finally fixed the code. Thanks Patrick.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,31 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var game = {
 	wins: 0,
 	losses: 0, 
-	guessesLeft: 9,
+	guessesLeft: 10,
 }
 
-document.onkeyup = function (event) {
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function compGuess() {
+	return letters[Math.floor(Math.random() * letters.length)];
+}
+
+var computerGuess = compGuess();
+
+document.onkeyup = function(event) {
 
 	var userGuess = event.key;
 
-	var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-
-	if ((userGuess = "a") || (userGuess = "b") || (userGuess = "c") || (userGuess = "d") || (userGuess = "e") || (userGuess = "f") || (userGuess = "g") || (userGuess = "h") || (userGuess = "i") || (userGuess = "j") || (userGuess = "k") || (userGuess = "l") || (userGuess = "m") || (userGuess = "n") || (userGuess = "o") || (userGuess = "p") || (userGuess = "q") || (userGuess = "r") || (userGuess = "s") || (userGuess = "t") || (userGuess = "u") || (userGuess = "v") || (userGuess = "w") || (userGuess = "x") || (userGuess = "y") || (userGuess = "z")) {
+	if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess ===  "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
 
 		if (userGuess === computerGuess) {
 			game.wins++;
 			console.log("You win! The letter guessed was " + computerGuess);
 			document.getElementById("guesses").textContent = " ";
-			game.guessesLeft = 9;
 			alert("You win! Would you like to play again and see if you can be stumped by my log?");
+			game.guessesLeft = 10;
+			computerGuess = compGuess();
 		}
 
 		else if (userGuess !== computerGuess) {
@@ -32,7 +38,9 @@ document.onkeyup = function (event) {
 			alert("You have been...stumped by my log. The letter you were trying to guess was " + computerGuess + " Would you like to try again?");
 			document.getElementById("guesses").textContent=" ";
 			console.log("You lost. The letter was " + computerGuess);
-			game.guessesLeft = 9;
+			game.guessesLeft = 10;
+			computerGuess = compGuess();
+
 		}
 	}
 
